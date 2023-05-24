75e7b6a2b3e62621b12b8cae68ab145efc97f686
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-24 14:30:25 -0300

    Inserindo novos exercícios

---
+++ 2 - Strings/4_processar_registro_marc.py
@@ -0,0 +1,91 @@
+# PROCESSAR REGISTRO MARC
+'''
+Desafio:
+
+Escreva um programa que leia o texto fornecido e exiba as seguintes informações:
+
+O título do livro, no campo MARC que começa com "245".
+O sobrenome e nome do autor, no campo MARC que começa com "100".
+O sobrenome e nome do tradutor, no campo MARC que começa com "700".
+O ano de publicação, no campo MARC que começa com "260".
+O número de páginas, no campo MARC que começa com "300".
+
+Certifique-se de utilizar manipulação de strings para extrair as informações corretas.
+
+Texto fornecido:
+
+LDR 00000        2200000   4500
+001 0123456789
+008 210524s2023    xx            000 0 eng d
+020 ## $a9780140449136 (paperback)
+100 1# $aDostoiévski, Fyodor,$d1821-1881.
+245 10$aCrime e Castigo /$cFyodor Dostoiévski ; traduzido por Constance Garnett.
+250 ## $aEdição especial.
+260 ## $aSão Paulo :$bEditora Exemplo,$c2023.
+300 ## $a500 páginas ;$c21 cm.
+336 ## $atext$btxt$2rdacontent
+337 ## $aunmediated$bn$2rdamedia
+338 ## $avolume$bnc$2rdacarrier
+500 ## $aInclui notas de rodapé.
+520 ## $a"Crime e Castigo" é um dos romances mais influentes da literatura mundial. Ele explora a mente atormentada de Raskólnikov, um jovem estudante que comete um assassinato e lida com as consequências emocionais e morais de seus atos.
+650 #0 $aRomance russo.
+650 #0 $aAssassinato$xFicção.
+700 1# $aGarnett, Constance,$d1861-1946,$etr.
+710 2# $aEditora Exemplo.
+
+
+'''
+
+registro_marc = '''LDR 00000        2200000   4500
+001 0123456789
+008 210524s2023    xx            000 0 eng d
+020 ## $a9780140449136 (paperback)
+100 1# $aDostoiévski, Fyodor,$d1821-1881.
+245 10$aCrime e Castigo /$cFyodor Dostoiévski ; traduzido por Constance Garnett.
+250 ## $aEdição especial.
+260 ## $aSão Paulo :$bEditora Exemplo,$c2023.
+300 ## $a500 páginas ;$c21 cm.
+336 ## $atext$btxt$2rdacontent
+337 ## $aunmediated$bn$2rdamedia
+338 ## $avolume$bnc$2rdacarrier
+500 ## $aInclui notas de rodapé.
+520 ## $a"Crime e Castigo" é um dos romances mais influentes da literatura mundial. Ele explora a mente atormentada de Raskólnikov, um jovem estudante que comete um assassinato e lida com as consequências emocionais e morais de seus atos.
+650 #0 $aRomance russo.
+650 #0 $aAssassinato$xFicção.
+700 1# $aGarnett, Constance,$d1861-1946,$etr.
+710 2# $aEditora Exemplo.
+'''
+
+
+def processar_registro_marc(texto):
+    titulo_inicio = texto.find("245 10$a") + len("245 10$a")
+    titulo_fim = texto.find("/$c", titulo_inicio)
+    titulo = texto[titulo_inicio:titulo_fim]
+
+    autor_inicio = texto.find("100 1# $a") + len("100 1# $a")
+    autor_fim = texto.find(",$d", autor_inicio)
+    autor = texto[autor_inicio:autor_fim]
+    
+    tradutor_inicio = texto.find("700 1# $a") + len("700 1# $a")
+    tradutor_fim = texto.find(",$d", tradutor_inicio)
+    tradutor = texto[tradutor_inicio:tradutor_fim]
+
+    ano_inicio = texto.find("260 ## $aSão Paulo :$bEditora Exemplo,$c") + len("260 ## $aSão Paulo :$bEditora Exemplo,$c")
+    ano_fim = ano_inicio + 4
+    ano = texto[ano_inicio:ano_fim]
+
+    paginas_inicio = texto.find("300 ## $a") + len("300 ## $a")
+    paginas_fim = texto.find(" páginas", paginas_inicio)
+    paginas = texto[paginas_inicio:paginas_fim]
+        
+
+    return titulo, autor, tradutor, ano, paginas
+
+registro_marc_processado = processar_registro_marc(registro_marc)
+
+print(f'''
+      Título: {registro_marc_processado[0]}
+      Autor: {registro_marc_processado[1]}
+      Tradutor: {registro_marc_processado[2]}
+      Ano: {registro_marc_processado[3]}
+      Páginas: {registro_marc_processado[4]}''')
---
+++ 2 - Strings/5_gerar_codigo_entrada.py
@@ -0,0 +1,56 @@
+# GERAR CÓDIGO ENTRADA 
+'''
+Desafio:
+
+Escreva um programa que gere um código de entrada de um documento, a partir do seu título, autoria, ano de publicação 
+e data/hora de entrada atuais.
+
+O código deverá conter todas as letras em maiúsculas e não deverá conter caracteres especiais e espaços em branco.
+O título deverá ser retornado de forma completa.
+A autoria deverá ser retornada apenas os 3 primeiros caracteres.
+
+Os quatro elementos do código (título, autoria, ano de publicação e data/hora de chegada) deverão ser separados por "_".
+
+Utilize as bibliotecas datetime e unicodedata.
+'''
+from datetime import datetime
+import unicodedata
+
+
+def remover_acentos(texto):
+    texto = unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('ASCII')
+    return texto
+
+def gerar_codigo_entrada(titulo, autor, ano_publicacao):
+    
+    #Tratamento inicial no título
+    titulo = remover_acentos(titulo)
+    titulo = titulo.strip().upper().replace(" ", "")
+
+    # Remover caracteres especiais
+    tabela_caracteres_especiais = str.maketrans("", "", '''!"#$%&'()*+-,./:;<=>?@[\]^_`{|}~''')
+    titulo = titulo.translate(tabela_caracteres_especiais)
+
+    #Tratamento inicial no Autor(a)
+    autor = remover_acentos(autor).upper()
+    autor = autor.translate(tabela_caracteres_especiais)
+    # Obter a data e hora atual
+    data_hora_chegada = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Gerar código completo de catalogação
+    codigo_completo = f"{titulo}_{autor[:3]}_{ano_publicacao}_{data_hora_chegada}"
+
+    return codigo_completo
+
+
+# Entrada do usuário
+titulo = input("Digite o título do documento: ")
+autor = input("Digite o nome do autor(a): ")
+ano_publicacao = input("Digite o ano de publicação: ")
+
+# Gerar código de catalogação
+codigo_catalogacao = gerar_codigo_entrada(titulo, autor, ano_publicacao)
+
+# Imprimir o código de catalogação
+print("Código de entrada:", codigo_catalogacao)
+
